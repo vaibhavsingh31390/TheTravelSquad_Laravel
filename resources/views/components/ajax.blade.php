72db2e156f945c10b025223e94c0120622536424
Vaibhav Singh <90971999+kineticwebworks@users.noreply.github.com>
2022-07-20 03:21:00 +0530

    Index Page Ajax Load More Media Variable Not Found Fixed

--- resources/views/components/ajax.blade.php
+++ resources/views/components/ajax.blade.php
@@ -4,7 +4,7 @@ if (isset($_POST['load_Data'])) {
     ?>
         @foreach ($allCards as $card)
         <div class="col">
-            @postCard(['route'=>'posts.show', 'id'=>$card->id, 'imageUrl'=>$card->media->path, 'title'=>$card->title,
+            @postCard(['route'=>'posts.show', 'id'=>$card->id, 'media'=>$card->media, 'path' => '$card->media->path', 'title'=>$card->title,
             'content'=>$card->content, 'createdAt'=>$card->created_at->diffForHumans(),
             'comments'=>$card->comments->count(), 'post'=>$card])
             @endpostCard
