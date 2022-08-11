144ade9970076a6f7b8dae18fe62f0b29ba4fd71
vaibhav.singhTM <vaibhav.singh@trulymadly.com>
2022-08-12 03:01:31 +0530

    Bug fixes & Added Like Acton Mail to Jobs Queue

--- app/Mail/TriggerLikeActionMail.php
+++ app/Mail/TriggerLikeActionMail.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class TriggerLikeActionMail extends Mailable
+class TriggerLikeActionMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
