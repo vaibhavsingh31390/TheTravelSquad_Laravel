d7d41bb7f954952585ff92757b0fd0880a268489
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2024-08-02 03:01:28 +0530

    [DEP] changes

--- database/seeders/MediaTableSeeder.php
+++ database/seeders/MediaTableSeeder.php
@@ -20,24 +20,25 @@ class MediaTableSeeder extends Seeder
         $posts = Posts::all();
         $users = \App\Models\User::all();
         $elemets = [$posts, $users];
-        if($users->count() === 0){
+        if ($users->count() === 0) {
             $this->command->info('There are no users, so no media will be added');
             return;
         }
-        if($posts->count() === 0){
+        if ($posts->count() === 0) {
             $this->command->info('There are no posts, so no media will be added');
             return;
         }
-        $images_Thumbnails = Storage::files('Sample_Thumbnails');
-        foreach($posts as $post){
+        $images_Thumbnails = Storage::disk('public')->files('Sample_Thumbnails');;
+        foreach ($posts as $post) {
             $randomImages = array_rand($images_Thumbnails);
             $randomImage = $images_Thumbnails[$randomImages];
             $post->media()->save(
                 Media::make(['path' => $randomImage])
             );
         };
-        $images_User_Thumbnails = Storage::files('Sample_Profile_Pictures');
-        foreach($users as $user){
+        $images_User_Thumbnails = Storage::disk('public')->files('Sample_Profile_Pictures');
+
+        foreach ($users as $user) {
             $randomImages = array_rand($images_User_Thumbnails);
             $randomImage = $images_User_Thumbnails[$randomImages];
             $user->media()->save(
